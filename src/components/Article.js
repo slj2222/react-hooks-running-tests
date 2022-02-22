ca6aa727aae2aa593a4c15f47b4e78aad876d51b
Spencer Johnson <sljohnson2222@gmail.com>
2022-02-22 09:42:07 -0600

    Completed assignment

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,9 +3,29 @@ import React from "react";
 function Article() {
   return (
     <div>
-      {/* please pass this test */}
+      please pass this test
     </div>
   );
 }
 
 export default Article;
+
+
+// test("displays the text 'please pass this test'", () => {
+//   render(<Article />);
+
+//   // add this line
+//   screen.debug();
+
+//   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+// });
+
+// test("displays the text 'please pass this test'", () => {
+//   render(<Article />);
+
+//   const element = screen.queryByText("please pass this test");
+
+//   screen.debug(element);
+
+//   expect(element).toBeInTheDocument();
+// });
